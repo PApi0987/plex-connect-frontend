85fd00f9a2d416f53de15f502e11b774be055587
PApi0987 <msigma997@gmail.com>
2026-01-10 13:04:58 +0100

    Create ThemeContext.js

---
+++ src/ThemeContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const toggleTheme = () => {
+    setThemeMode(prev => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ themeMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
