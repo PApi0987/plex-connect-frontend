f2ca72bff3efc3c7104f3519df1f36516fe5596a
PApi0987 <msigma997@gmail.com>
2026-01-10 12:58:32 +0100

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+  background: "#FFFFFF",
+  text: "#333333",
+  primary: "#D3D3D3",
+  accent: "#003366",
+  buttonText: "#FFFFFF"
+};
+
+export const darkTheme = {
+  background: "#000000",
+  text: "#FFFFFF",
+  primary: "#003366",
+  accent: "#555555",
+  buttonText: "#FFFFFF"
+};
