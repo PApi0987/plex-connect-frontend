8a83b30f7d96bf11a7000724ce92e1bc8e54419e
PApi0987 <msigma997@gmail.com>
2026-01-10 13:18:26 +0100

    Update AuthContext.js

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -1,16 +1,11 @@
-exportrtrt { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  const login = (email, password) => {
-    setUser({ name: "Prosper Bassey", email });
-  };
-
+  const [user, setUser] = useState({ name: "Prosper" });
+  const login = (name) => setUser({ name });
   const logout = () => setUser(null);
-
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
