44fcd61a5edcb45a298b1909c0b42360a5145d72
PApi0987 <msigma997@gmail.com>
2026-01-10 13:17:36 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ThemeProviderWrapper } from "./contexts/ThemeContext";
+
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <AuthProvider>
+      <ThemeProviderWrapper>
+        <App />
+      </ThemeProviderWrapper>
+    </AuthProvider>
+  </BrowserRouter>
+);
