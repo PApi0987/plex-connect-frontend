ca0228018d842a6b6835946f1e0b6f8986c6bc09
PApi0987 <msigma997@gmail.com>
2026-01-17 23:04:05 +0100

    Wrap app rendering in React.StrictMode

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
+// --- Context Providers ---
 import { AuthProvider } from "./contexts/AuthContext";
 import { ThemeProviderWrapper } from "./contexts/ThemeContext";
 
+// --- Global Styles ---
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
-    <AuthProvider>
-      <ThemeProviderWrapper>
-        <App />
-      </ThemeProviderWrapper>
-    </AuthProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ThemeProviderWrapper>
+          <App />
+        </ThemeProviderWrapper>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
